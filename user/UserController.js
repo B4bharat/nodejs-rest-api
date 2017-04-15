ddc3b5644882a2fa7ef3d99efd93ae3dc1f02ed1
Bharat Poptwani <b4bharat.available@gmail.com>
2017-04-15 23:26:48 +0530

    basic skeleton of router for 'User' model created

---
+++ user/UserController.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({
+  'extended': true
+}));
+
+var User = require('./user');
+
+module.exports = router;
